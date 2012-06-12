5492ffa19f0a2062cad5a6be3dd4412b6baca548
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-06-11 20:14:27 -0500

    a

--- carrot-conf-base/src/test/java/com/carrotgarden/conf/base/TestInOSGI.java
+++ carrot-conf-base/src/test/java/com/carrotgarden/conf/base/TestInOSGI.java
@@ -42,6 +42,7 @@ public class TestInOSGI {
 
 				junitBundles(),
 
+				repository("http://repo1.maven.org/").id("central"),
 				repository("http://download.eclipse.org/jgit/maven").id("jgit"),
 
 				mavenBundle().groupId("com.carrotgarden.osgi")
