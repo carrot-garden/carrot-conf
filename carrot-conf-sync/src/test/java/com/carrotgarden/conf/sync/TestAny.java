fa5afe97f038241f9adb4c7d7f078a2dc63ce756
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-09-09 12:59:07 -0500

    a

--- carrot-conf-sync/src/test/java/com/carrotgarden/conf/sync/TestAny.java
+++ carrot-conf-sync/src/test/java/com/carrotgarden/conf/sync/TestAny.java
@@ -30,7 +30,7 @@ public class TestAny {
 
 	protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	private static final String PAX_LEVEL = "DEBUG";
+	private static final String PAX_LEVEL = "ERROR";
 
 	@Configuration
 	public Option[] config() {
